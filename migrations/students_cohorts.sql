71200146e1fba8749debe5670800079c87cba2e8
getenesh <getenesh.guta@gmail.com>
2023-04-19 21:21:55 -0400

    create tables

---
+++ migrations/students_cohorts.sql
@@ -0,0 +1,17 @@
+CREATE TABLE cohorts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  cohort_name VARCHAR(255) NOT NULL,
+  cohort_start_date DATE,
+  cohort_end_date DATE
+);
+
+CREATE TABLE students (
+  id SERIAL PRIMARY KEY NOT NULL,
+  student_name VARCHAR(255),
+  email VARCHAR(255),
+  phone VARCHAR(32),
+  githhub VARCHAR(255),
+  cohort_start_date DATE,
+  cohort_end_date DATE,
+  cohort_id INTEGER References cohorts(id) ON DELETE CASCADE
+);
